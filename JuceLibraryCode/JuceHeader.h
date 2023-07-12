6965e7797c1afc2f0230c7aed8c7495a70cfddb4
Emmet Hayes <ehayes1@alumni.berklee.edu>
2023-07-11 20:45:44 -0400

    Updated .jucer file to include company name

--- JuceLibraryCode/JuceHeader.h
+++ JuceLibraryCode/JuceHeader.h
@@ -43,7 +43,7 @@
 namespace ProjectInfo
 {
     const char* const  projectName    = "HayesCompressor";
-    const char* const  companyName    = "";
+    const char* const  companyName    = "EmmetSoft";
     const char* const  versionString  = "1.0.0";
     const int          versionNumber  = 0x10000;
 }
